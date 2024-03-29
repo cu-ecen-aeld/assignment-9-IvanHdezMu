a0b49a1f7e230469cb19942a1d6cf29890cb4965
Ivan Hernandez <ihm128@gmail.com>
2024-03-29 17:22:35 +0100

    tarea 9

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 5e269cfcf5e19a970cd333be0a56665095232de9
+AESD_ASSIGNMENTS_VERSION = 318550ef00849fc8b9c391250fc73022ec9c583a
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
